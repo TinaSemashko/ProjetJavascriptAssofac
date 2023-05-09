e69a65b425135998e98d6b3cb97966ae80a9e476
TinaSemashko <akateria.satory@gmail.com>
2023-05-09 23:10:51 +0200

    pages

--- pages/products.js
+++ pages/products.js
@@ -1,6 +1,3 @@
-const containerPrecise = document.querySelector(".container-prod-precise");
-let cell = ``;
-let buttons = [];
 const product = {
   id: "",
   title: "",
@@ -8,6 +5,45 @@ const product = {
   quantity: 0,
 };
 
+const GridCell = (array, container) => {
+  let cell = ``;
+  let buttons = [];
+  array.forEach((element, index) => {
+    cell =
+      cell +
+      `
+        <div class="cell">
+        <div class="text-image">${element[0]}        
+            <br />
+            <br />
+        </div>
+        <img
+            src=${element[1]}
+            class="img-fluid"
+            alt="..."
+        />
+        <button class="add-to-basket${index}">
+            ajouter au panier
+        </button>
+        </div> `;
+  });
+  container.innerHTML = cell;
+
+  array.forEach((element, index) => {
+    let myButton = buttons[index];
+    myButton = document.querySelector(".add-to-basket" + index);
+    myButton.addEventListener("click", () => addToBasket(element[0]));
+  });
+};
+
+//test
+const addToBasket = (prod) => {
+  alert("testt " + prod);
+};
+/////
+
+//precise
+const containerPrecise = document.querySelector("#precise");
 const precise = [
   ["Rubys", "../images/ruby-stone-gb210f9d4a_1280.jpg"],
   ["Saphires", "../images/blue-gems-g2b821c703_1280.jpg"],
@@ -16,35 +52,64 @@ const precise = [
   ["Vente en gros", "../images/pexels-murilo-fonseca-10582459.jpg"],
   ["Émeraudes", "../images/pexels-jimmy-chan-13648455.jpg"],
 ];
+if (containerPrecise) GridCell(precise, containerPrecise);
 
-const addToBasket = (prod) => {
-  alert("testt " + prod);
-};
+//articles
+const containerArticles = document.querySelector("#articles");
+const articles = [
+  ["Rubys", "../images/pexels-dima-valkov-3266700.jpg"],
+  ["Saphires", "../images/pexels-e-pro-1573236.jpg"],
+  ["Pierres brutes", "../images/pexels-jacqueline-kabrel-10499788.jpg"],
+  ["Diamants", "../images/pexels-michelle-leman-6765812.jpg"],
+  ["Vente en gros", "../images/"],
+  ["Émeraudes", "../images/"],
+  ["Émeraudes", "../images/"],
+  ["Émeraudes", "../images/"],
+];
 
-precise.forEach((element, index) => {
-  cell =
-    cell +
-    `
-    <div class="cell">
-    <div class="text-image">${element[0]}        
-        <br />
-        <br />
-    </div>
-    <img
-        src=${element[1]}
-        class="img-fluid"
-        alt="..."
-    />
-    <button class="add-to-basket${index}">
-        ajouter au panier
-    </button>
-    </div> `;
-});
-containerPrecise.innerHTML = cell;
+if (containerArticles) GridCell(articles, containerArticles);
 
-precise.forEach((element, index) => {
-  let myButton = buttons[index];
-  myButton = document.querySelector(".add-to-basket" + index);
-  console.log(myButton);
-  myButton.addEventListener("click", () => addToBasket(element[0]));
-});
+//biju
+const containerBiju = document.querySelector("#biju");
+const biju = [
+  ["Rubys", "../images/jewellery-gb278554cc_1280.jpg"],
+  ["Saphires", "../images/pexels-ecrin-7992686.jpg"],
+  ["Pierres brutes", "../images/pexels-git-stephen-gitau-1670723.jpg"],
+  ["Diamants", "../images/pexels-hatice-genç-14856437.jpg"],
+  ["Vente en gros", "../images/pexels-marta-branco-1395306.jpg"],
+  ["Émeraudes", "../images/"],
+  ["Émeraudes", "../images/"],
+  ["Émeraudes", "../images/"],
+];
+
+if (containerBiju) GridCell(biju, containerBiju);
+
+//orniment
+const containerOrniment = document.querySelector("#orniment");
+const orniment = [
+  ["Rubys", "../images/first.jpg"],
+  ["Saphires", "../images/HealingCrystals_Com.jpg"],
+  ["Pierres brutes", "../images/"],
+  ["Diamants", "../images/"],
+  ["Vente en gros", "../images/"],
+  ["Émeraudes", "../images/"],
+  ["Émeraudes", "../images/"],
+  ["Émeraudes", "../images/"],
+];
+
+if (containerOrniment) GridCell(orniment, containerOrniment);
+
+//semi
+const containerSemi = document.querySelector("#semi");
+const semi = [
+  ["Rubys", "../images/multi pierres.jpg"],
+  ["Saphires", "../images/pexels-abdul-matloob-13300169.jpg"],
+  ["Pierres brutes", "../images/pexels-abdul-matloob-13300951.jpg"],
+  ["Diamants", "../images/pexels-abdul-matloob-13303468.jpg"],
+  ["Vente en gros", "../images/pexels-arjiv-exports-5378800.jpg"],
+  ["Émeraudes", "../images/pexels-karolina-grabowska-4040587.jpg"],
+  ["Émeraudes", "../images/"],
+  ["Émeraudes", "../images/"],
+];
+
+if (containerSemi) GridCell(semi, containerSemi);
